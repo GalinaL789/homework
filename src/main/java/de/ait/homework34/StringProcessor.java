93c6a459b5652d26b5c9989316082a32451eb64c
AIT TR Student <rusgal37@gmail.com>
2023-10-09 20:16:55 +0200

    34: Project is created, add pom.xml and Class for test

--- src/main/java/de/ait/homework34/StringProcessor.java
+++ src/main/java/de/ait/homework34/StringProcessor.java
@@ -2,7 +2,8 @@ package de.ait.homework34;
 
 public class StringProcessor {
 
-    public String concatenate(String str1, String str2) {
+    public String concatenate(String str1, String str2)
+    {
         return str1 + str2;
     }
 
--- src/test/java/de/ait/homework34/StringProcessorTest.java
+++ src/test/java/de/ait/homework34/StringProcessorTest.java
@@ -1,32 +1,35 @@
 package de.ait.homework34;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringProcessorTest {
-    package de.ait.homework34;
-
-    public class StringProcessor {
-
-        public String concatenate(String str1, String str2) {
-            return str1 + str2;
-        }
-
-        public int getLength(String str) {
-            return str.length();
-        }
-
-        public String reverse(String str) {
-            return new StringBuilder(str).reverse().toString();
-        }
-
-        public boolean isPalindrome(String str) {
-            String reversed = reverse(str);
-            return str.equals(reversed);
-        }
+    private StringProcessor stringProcessor;
+    @BeforeEach
+    public void setUp() {
+        stringProcessor = new StringProcessor();
     }
-
-
-
-
-
-
+    @Test
+    @DisplayName("Проверка соединения строк")
+    public void testConcatenate() {
+        assertEquals("Good Day", stringProcessor.concatenate("Good ", "Day"));
+    }
+    @Test
+    @DisplayName("Checking the length of the String")
+    public void testGetLength(){
+        assertEquals(6,stringProcessor.getLength("Galina"));
+    }
+    @Test
+    @DisplayName("Check of inverse String")
+    public void testReverse(){
+        assertEquals("oksaL",stringProcessor.reverse("Lasko"));
+    }
+    @Test
+    @DisplayName("Check polindrome")
+    public void testIsPalindrome(){
+        assertEquals(true,stringProcessor.isPalindrome("racecar"));
+    }
 
 }
